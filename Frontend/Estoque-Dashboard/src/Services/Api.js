42e18da68a9767fe14a0dc0dbfcd64e17ba896e6
Erick Caetano <erick.caetano1200@gmail.com>
2026-09-22 18:10:11 +0000

    Ajuste de URI

--- Frontend/Estoque-Dashboard/src/Services/Api.js
+++ Frontend/Estoque-Dashboard/src/Services/Api.js
@@ -1,7 +1,9 @@
 import axios, { Axios } from "axios";
 
 const api = axios.create({
-  baseURL:  `http://localhost:8081` ,
+ // baseURL:  `http://localhost:8081` ,//
+    baseURL:  `https://sistema-estoque-8p4a.onrender.com` 
+
 });
 
 export default api;
